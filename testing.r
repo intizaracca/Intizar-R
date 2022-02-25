daea7743d1af9e91a6f8bbf242bd876d57500ef5
Muhammad Intizar Ghafoor <intizar.acca@gmail.com>
2022-02-25 14:32:43 +0500

    5.1 to 5.3 classes

--- testing.r
+++ testing.r
@@ -7,3 +7,33 @@ print(name)
 print('I am here')
 All= 2+5
 print(All)
+A=5
+class(A)
+
+b=6L
+class(b)
+print(b)
+x <- 1
+print(x)
+y<- c(1.7, 's') ## Mixing Objectives
+y
+f<- c(True, 2)
+class(f)
+y<-c('a','TRUE')
+y
+y<- c(TRUE, 2)
+y
+## Explicit Coercion
+x<- 1:6
+class(x)
+as.numeric(x)
+x<- 0:6
+as.logical(x)
+x<- 0:6
+as.character(x)
+x<- c('a','b','c')
+as.numeric(x)
+x<- 0:6
+as.complex(x)
+x<-2
+x
